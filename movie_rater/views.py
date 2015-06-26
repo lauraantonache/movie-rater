e734f6bd4c94f68eed2e5134ee8e4ed2bcacc678
Laura Antonache <lauraantonache@gmail.com>
2015-06-26 09:34:27 +0300

    Working on the HTML, CSS part

--- movie_rater/views.py
+++ movie_rater/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . models import Movie
+from django.utils import timezone
 
 # Create your views here.
 def movie_list(request):
-	return render(request, 'movie_rater/movie_list.html', {})
+	movies = Movie.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+	return render(request, 'movie_rater/movie_list.html', {'movies':movies})
