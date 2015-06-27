9b2650a482d2e48294cab30e03bd5a00241fb135
Laura Antonache <lauraantonache@gmail.com>
2015-06-27 13:37:45 +0300

    Done a form to be able to add new movies. Still have problems with the authentification

--- movie_rater/urls.py
+++ movie_rater/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.movie_list),
 	url(r'^movie/(?P<pk>[0-9]+)/$', views.movie_detail),
+	 url(r'^movie/new/$', views.new_movie, name='new_movie'),
 ]
--- movie_rater/views.py
+++ movie_rater/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Movie
+from . forms import MovieForm
 from django.utils import timezone
 
 # Create your views here.
@@ -10,3 +11,16 @@ def movie_list(request):
 def movie_detail(request, pk):
 	movie = get_object_or_404(Movie, pk=pk)
 	return render(request, 'movie_rater/movie_detail.html', {'movie': movie})
+	
+def new_movie(request):
+	if request.method == "POST":
+		form = MovieForm(request.POST)
+		if form.is_valid():
+			movie = form.save(commit=False)
+			movie.author = request.user
+			movie.created_date = timezone.now()
+			movie.save()
+			return redirect('movie_rater.views.movie_detail', pk=movie.pk)
+	else:
+		form = MovieForm()
+	return render(request, 'movie_rater/movie_edit.html', {'form': form})
