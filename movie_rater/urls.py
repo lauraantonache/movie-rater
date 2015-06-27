5293a00ee47c2444ddebf599649f5bcc7cbf2d90
Laura Antonache <lauraantonache@gmail.com>
2015-06-28 01:11:21 +0300

    introduced information about the loged in user on the page

--- movie_rater/urls.py
+++ movie_rater/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.movie_list),
 	url(r'^movie/(?P<pk>[0-9]+)/$', views.movie_detail),
-	 url(r'^movie/new/$', views.new_movie, name='new_movie'),
+	url(r'^movie/new/$', views.new_movie, name='new_movie'),
+	url(r'^login/$', views.login_view), 
 ]
--- movie_rater/views.py
+++ movie_rater/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from . models import Movie
 from . forms import MovieForm
 from django.utils import timezone
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 def movie_list(request):
@@ -16,11 +18,26 @@ def new_movie(request):
 	if request.method == "POST":
 		form = MovieForm(request.POST)
 		if form.is_valid():
-			movie = form.save(commit=False)
-			movie.author = request.user
-			movie.created_date = timezone.now()
-			movie.save()
-			return redirect('movie_rater.views.movie_detail', pk=movie.pk)
+			if request.user.is_authenticated():
+				movie = form.save(commit=False)
+				movie.author = request.user
+				movie.created_date = timezone.now()
+				movie.save()
+				return redirect('movie_rater.views.movie_detail', pk=movie.pk)
 	else:
 		form = MovieForm()
 	return render(request, 'movie_rater/movie_edit.html', {'form': form})
+	
+
+def login_view(request, ):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('movie_rater.views.new_movie')
+        else:
+            return redirect('movie_rater.views.login_view')
+    else:
+         return redirect('movie_rater.views.login_view')
